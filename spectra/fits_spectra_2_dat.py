9f23db78338d701f66127a6bd2927939640809a0
Marco Gulino <marco@gulinux.net>
2015-11-27 23:11:11 +0100

    Add background button

--- spectra/fits_spectra_2_dat.py
+++ spectra/fits_spectra_2_dat.py
@@ -19,7 +19,14 @@ class DisplayImage:
     self.brotate = Button(self.image_view.figure.add_axes([0, 0.95, 0.1, 0.05]), "Rotate")
     self.bselect_background = Button(self.image_view.figure.add_axes([0.1, 0.95, 0.25, 0.05]), "Select Background")
     self.brotate.on_clicked(self.rotate)
-    self.bselect_background.on_clicked(self.rotate)
+    self.bselect_background.on_clicked(self.select_bg)
+    
+    self.RS = RectangleSelector(self.image_view, self.,
+                                       drawtype='box', useblit=True,
+                                       button=[1, 3],  # don't use middle button
+                                       minspanx=5, minspany=5,
+                                       spancoords='pixels',
+                                       interactive=True)
     
     plt.figure()
     self.image_plot = plt.axes()
@@ -27,11 +34,20 @@ class DisplayImage:
     self.draw_plot()
     plt.show()
     
+  def bg_selected(eclick, erelease):
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+    print("(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2))
+    print(" The button you used were: %s %s" % (eclick.button, erelease.button))
+    
   def draw_plot(self):
     sums = self.rotated.sum(0)
     self.image_plot.clear()
     self.image_plot.plot(sums)
     self.image_plot.figure.canvas.draw()
+    
+  def select_bg(self, evt):
+    self.RS.set_active(True)
     
   def rotate(self, evt):
     self.degrees = QInputDialog.getDouble(None, "Rotate", "Enter degrees for image rotation", self.degrees[0], 0, 360, 2)
