5c043f7777c5159f6c0dfe0aa51cdc55a4ae47e8
Marco Gulino <marco@gulinux.net>
2015-11-27 23:11:11 +0100

    print image information

--- spectra/fits_spectra_2_dat.py
+++ spectra/fits_spectra_2_dat.py
@@ -19,7 +19,7 @@ for hdu in image_hdu:
   print(repr(hdu.header))
 
 img_data = image_hdu[0].data
-print type(img_data)
+print("Type: {}, size: {}, ndim: {}, dtype: {}, shape: {}".format(type(img_data),img_data.size, img_data.ndim, img_data.dtype, img_data.shape) )
 
 cv2.imshow('image', img_data)
 cv2.waitKey(0)
