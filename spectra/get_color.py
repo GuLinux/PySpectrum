5bbdd328e9832e1bad78eb541f2f330949571818
Marco Gulino <marco@gulinux.net>
2015-11-27 23:11:11 +0100

    Updated colormap to jet

--- spectra/get_color.py
+++ spectra/get_color.py
@@ -4,7 +4,7 @@ from PyQt5.QtGui import *
 import sys
 
 def get_color(value):
-    return plt.cm.Spectral(value)
+    return plt.cm.jet(value)
 
 app = QApplication(sys.argv)
 
@@ -12,7 +12,7 @@ app = QApplication(sys.argv)
 img = QImage(1000, 100, QImage.Format_ARGB32)
 
 for i in range(0, 1000):
-    color = get_color(i/1000.0)
+    color = get_color((1000-i)/1000.0)
     qcolor = QColor(color[0]*255, color[1]*255, color[2]*255)  
     for h in range(0, 100):
       img.setPixel(i, h, qcolor.rgba())
