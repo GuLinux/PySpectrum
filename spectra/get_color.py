85fb6a8ac567d7b1c112233d61855c224906080b
Marco Gulino <marco@gulinux.net>
2015-11-27 23:11:11 +0100

    Spectral demo

---
+++ spectra/get_color.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import sys
+
+def get_color(value):
+    return plt.cm.Spectral(value)
+
+app = QApplication(sys.argv)
+
+
+img = QImage(1000, 100, QImage.Format_ARGB32)
+
+for i in range(0, 1000):
+    color = get_color(i/1000.0)
+    qcolor = QColor(color[0]*255, color[1]*255, color[2]*255)  
+    for h in range(0, 100):
+      img.setPixel(i, h, qcolor.rgba())
+
+
+label = QLabel()
+label.setPixmap(QPixmap.fromImage(img))
+label.show()
+sys.exit(app.exec())
