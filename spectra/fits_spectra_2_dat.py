d6cab25f631e41005ea23665679f8ca832f92072
Marco Gulino <marco@gulinux.net>
2015-11-27 23:11:11 +0100

    save file name

--- spectra/fits_spectra_2_dat.py
+++ spectra/fits_spectra_2_dat.py
@@ -10,18 +10,23 @@ import scipy.ndimage.interpolation
 from matplotlib.widgets import *
 
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QSettings
+import os
 
 class DisplayImage:
   def __init__(self, image):
+    self.config = QSettings('GuLinux', 'PySpectra')
     self.original = image.astype(float)
     self.rotated = self.original
+    self.background = np.zeros((1,1), float)
     self.image_view = plt.imshow(self.original, cmap='gray')
     self.brotate = Button(self.image_view.figure.add_axes([0, 0.95, 0.1, 0.05]), "Rotate")
     self.bselect_background = Button(self.image_view.figure.add_axes([0.1, 0.95, 0.25, 0.05]), "Select Background")
+    self.bsave = Button(self.image_view.figure.add_axes([0.1 + 0.25, 0.95, 0.1, 0.05]), "Save")
     self.brotate.on_clicked(self.rotate)
+    self.bsave.on_clicked(self.save)
     self.bselect_background.on_clicked(self.select_bg)
-    print(dir(self.image_view))
-    self.RS = RectangleSelector(self.image_view.figure.subplot(), self.bg_selected,
+    self.RS = RectangleSelector(self.image_view.axes, self.bg_selected,
                                        drawtype='box', useblit=True,
                                        button=[1, 3],  # don't use middle button
                                        minspanx=5, minspany=5,
@@ -34,14 +39,18 @@ class DisplayImage:
     self.draw_plot()
     plt.show()
     
-  def bg_selected(eclick, erelease):
+  def bg_selected(self, eclick, erelease):
+    print(eclick, erelease)
     x1, y1 = eclick.xdata, eclick.ydata
     x2, y2 = erelease.xdata, erelease.ydata
+    self.background = self.rotated[x1:x2, y1:y2]
     print("(%3.2f, %3.2f) --> (%3.2f, %3.2f)" % (x1, y1, x2, y2))
-    print(" The button you used were: %s %s" % (eclick.button, erelease.button))
+    print("mean background: %f" % self.background.mean())
+    self.RS.set_active(False)
+    self.draw_plot()
     
   def draw_plot(self):
-    sums = self.rotated.sum(0)
+    sums = (self.rotated - self.background.mean()).sum(0)
     self.image_plot.clear()
     self.image_plot.plot(sums)
     self.image_plot.figure.canvas.draw()
@@ -57,6 +66,14 @@ class DisplayImage:
     self.image_view.set_data(self.rotated)
     self.image_view.figure.canvas.draw()
     self.draw_plot()
+    
+  def save(self, ev):
+    save_file = QFileDialog.getSaveFileName(None, "Save plot...", self.config.value('last_save_dir'), "*FITS file (.fit *.fits)")
+    if not save_file[0]:
+      return
+    filename = save_file[0]
+    self.config.setValue('last_save_dir', os.path.dirname(os.path.realpath(filename)))
+    print(filename)
     
     
 if len(sys.argv) < 2:
