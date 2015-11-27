e98b8ba75564dbcfd6997a14afe70a6c8aec836f
Marco Gulino <marco@gulinux.net>
2015-11-27 23:11:11 +0100

    Open file

--- PySpectrum/import_image.py
+++ PySpectrum/import_image.py
@@ -2,6 +2,7 @@ from ui_import_image import Ui_ImportImage
 from PyQt5.QtWidgets import QWidget
 
 class ImportImage(QWidget):
-    def __init__(self):
+    def __init__(self, fits_file):
         super(ImportImage, self).__init__()
+        self.fits_file = fits_file
 
--- PySpectrum/pyspectrum_main_window.py
+++ PySpectrum/pyspectrum_main_window.py
@@ -1,9 +1,18 @@
 from ui_pyspectrum_main_window import Ui_PySpectrumMainWindow
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 from import_image import ImportImage
+from PyQt5.QtCore import QSettings
 
 class PySpectrumMainWindow(QMainWindow):
     def __init__(self):
         super(PySpectrumMainWindow, self).__init__()
         self.ui = Ui_PySpectrumMainWindow()
         self.ui.setupUi(self)
+        self.settings = QSettings("GuLinux", "PySpectrum")
+        self.ui.actionOpen_Image.triggered.connect(self.open_image)
+        
+    def open_image(self):
+        file = QFileDialog.getOpenFileName(self, "Open FITS Image", self.settings.value("last_open_image_dir", str), "FITS Images (*.fit *.fits)")[0]
+        if not file:
+            return
+        print(file)
