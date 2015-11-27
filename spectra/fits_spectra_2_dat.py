d24d1cb2ffc334e0b5f01743362ebe6d9088ed18
Marco Gulino <marco@gulinux.net>
2015-11-27 23:11:11 +0100

    Showing secondary graph

--- spectra/fits_spectra_2_dat.py
+++ spectra/fits_spectra_2_dat.py
@@ -9,8 +9,29 @@ import time
 import scipy.ndimage.interpolation
 from matplotlib.widgets import Button
 
+from PyQt5.QtWidgets import *
 
-
+class DisplayImage:
+  def __init__(self, image):
+    self.original = image
+    self.rotated = image
+    self.plot = plt.imshow(self.original, cmap='gray')
+    self.subplot = plt.figure()
+    plt.figure(self.plot.figure.number)
+    self.brot_ax = plt.axes([0, 0.95, 0.1, 0.05])
+    self.brotate = Button(self.brot_ax, "Rotate")
+    self.brotate.on_clicked(self.rotate)
+    self.degrees = (0, True)
+    plt.show()
+    
+  def rotate(self, evt):
+    self.degrees = QInputDialog.getDouble(None, "Rotate", "Enter degrees for image rotation", self.degrees[0])
+    if not self.degrees[1]:
+      return
+    self.rotated = scipy.ndimage.interpolation.rotate(self.original, self.degrees[0])
+    self.plot.set_data(self.rotated)
+    plt.draw()
+    
 if len(sys.argv) < 2:
   print >> sys.stderr, "Usage: " + sys.argv[0] + " image.fit"
   sys.exit(1)
@@ -25,12 +46,4 @@ for hdu in image_hdu:
 img_data = image_hdu[0].data
 print("Type: {}, size: {}, ndim: {}, dtype: {}, shape: {}".format(type(img_data),img_data.size, img_data.ndim, img_data.dtype, img_data.shape) )
 
-degrees = 0
-#rotated = scipy.ndimage.interpolation.rotate(img_data, 20)
-#toolbar = plt.NavigationToolbar()
-
-plot = plt.imshow(img_data, cmap='gray')
-brot_ax = plt.axes([0, 0.95, 0.1, 0.05])
-brotate = Button(brot_ax, "Rotate")
-brotate.on_clicked(lambda pos:print("foo %d" % pos.button))
-plt.show()
+display_image = DisplayImage(img_data)
