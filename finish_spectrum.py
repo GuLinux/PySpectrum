f43cca943e2db03fa0eb0e54f013b13b849cacc0
Marco Gulino <marco@gulinux.net>
2015-12-02 00:58:49 +0100

    Fixed color synthesis

--- finish_spectrum.py
+++ finish_spectrum.py
@@ -15,6 +15,56 @@ import matplotlib as plt
 import numpy as np
 import math
 
+import sys
+import os
+import traceback
+import optparse
+import time
+import logging
+
+
+#credits: http://www.noah.org/wiki/Wavelength_to_RGB_in_Python
+def wavelength_to_rgb(wavelength, flux, gamma=0.8):
+
+    '''This converts a given wavelength of light to an 
+    approximate RGB color value. The wavelength must be given
+    in nanometers in the range from 380 nm through 750 nm
+    (789 THz through 400 THz).
+
+    Based on code by Dan Bruton
+    http://www.physics.sfasu.edu/astro/color/spectra.html
+    '''
+    w=wavelength
+    if w >= 380 and w < 440:
+        R = -(w - 440.) / (440. - 380.)
+        G = 0.0
+        B = 1.0
+    elif w >= 440 and w < 490:
+        R = 0.0
+        G = (w - 440.) / (490. - 440.)
+        B = 1.0
+    elif w >= 490 and w < 510:
+        R = 0.0
+        G = 1.0
+        B = -(w - 510.) / (510. - 490.)
+    elif w >= 510 and w < 580:
+        R = (w - 510.) / (580. - 510.)
+        G = 1.0
+        B = 0.0
+    elif w >= 580 and w < 645:
+        R = 1.0
+        G = -(w - 645.) / (645. - 580.)
+        B = 0.0
+    elif w >= 645 and w <= 780:
+        R = 1.0
+        G = 0.0
+        B = 0.0
+    else:
+        R = 0.0
+        G = 0.0
+        B = 0.0
+    return [R, G, B, flux]
+
 class FinishSpectrum(QWidget):
     def __init__(self, fits_file, settings, database):
         super(FinishSpectrum, self).__init__()
@@ -88,7 +138,9 @@ class FinishSpectrum(QWidget):
         self.profile_plot.plot(self.spectrum.wavelengths, self.spectrum.fluxes)
 
         self.synthetize.axes.set_axis_bgcolor('black')
-        colors = [FinishSpectrum.synthetize(w, self.spectrum.fluxes[i]) for i,w in enumerate(self.spectrum.wavelengths)]
+        #colors = [FinishSpectrum.synthetize(w, self.spectrum.fluxes[i]) for i,w in enumerate(self.spectrum.wavelengths)]
+        f_fluxes = lambda f: math.pow(f, 3/5)
+        colors = [wavelength_to_rgb(w/10., f_fluxes(self.spectrum.fluxes[i])) for i,w in enumerate(self.spectrum.wavelengths)]
         im_height = 150
         colors = np.array(colors*im_height).reshape(im_height,len(colors),4)
         self.synthetize.imshow(colors, extent=[self.spectrum.wavelengths[0], self.spectrum.wavelengths[-1], 0, im_height])
