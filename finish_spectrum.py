66aa2dbc286b048276c976ca60238dd85027c7b9
Marco Gulino <marco@gulinux.net>
2015-12-01 19:29:26 +0100

    better save options

--- finish_spectrum.py
+++ finish_spectrum.py
@@ -28,7 +28,6 @@ class FinishSpectrum(QWidget):
         remove_action.menu().addAction("Before point", lambda: self.remove('before'))
         remove_action.menu().addAction("After point", lambda: self.remove('after'))
         self.toolbar.addSeparator()
-        
         self.reference_spectra_dialog = ReferenceSpectraDialog(database)
         self.reference_spectra_dialog.fits_picked.connect(self.open_reference)
         self.lines_dialog = LinesDialog()
@@ -41,11 +40,14 @@ class FinishSpectrum(QWidget):
         self.toolbar.addAction('Spectral Lines', lambda: self.lines_dialog.show())
         
         self.toolbar.addSeparator()
-        self.toolbar.addAction("Export Image...", lambda: QtCommons.save_file('Export plot to image', 'PNG (*.png);;PDF (*.pdf);;PostScript (*.ps);;SVG (*.svg)', lambda f: self.spectrum_plot.figure.savefig(f[0], bbox_inches='tight')))
+        self.toolbar.addAction("Export Image...", lambda: QtCommons.save_file('Export plot to image', 'PNG (*.png);;PDF (*.pdf);;PostScript (*.ps);;SVG (*.svg)', lambda f: self.spectrum_plot.figure.savefig(f[0], bbox_inches='tight', dpi=300)))
         self.draw()
         save_action = self.toolbar.addAction(QIcon.fromTheme('document-save'), 'Save', lambda: QtCommons.save_file('Save plot...', 'FITS file (.fit)', self.save, self.settings.value('last_save_plot_dir')))
     def draw(self):
         self.spectrum_plot.plot(self.spectrum.wavelengths, self.spectrum.fluxes)
+        self.spectrum_plot.axes.set_xlabel('lambda (Å)')
+        self.spectrum_plot.axes.set_ylabel('relative flux')
+        self.spectrum_plot.figure.canvas.draw()
         
     def instrument_response(self, filename):
         instrument_response_file = fits.open(filename)
