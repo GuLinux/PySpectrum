2c35ebec6512bd1d8abefee0ea0de0f08ba1e4b8
Marco Gulino <marco@gulinux.net>
2015-12-03 23:37:23 +0100

    add project class skeleton

---
+++ project.py
@@ -0,0 +1,23 @@
+
+class Project:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+        
+    def raw_profiles(self):
+        pass
+    
+    def calibrated_profiles(self):
+        pass
+    
+    def instrument_responses(self):
+        pass
+    
+    def finished(self):
+        pass
+    
+    def rotation_angle(self):
+        pass
+    
+    def dispersion(self):
+        pass
