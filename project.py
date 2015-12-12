9354c746f827110c1f1b4127d41f6f24e21aae11
Marco Gulino <marco@gulinux.net>
2015-12-12 19:56:13 +0100

    use dict in profile

--- project.py
+++ project.py
@@ -4,47 +4,55 @@ from PyQt5.QtCore import QDate, Qt
 
 class Project:
     def __init__(self, path='', file=None):
+        self.data = {}
         self.set_path(path)
-        self.__init_fields()
         if self.path:
             with open(self.__projectfile()) as data_file:
-                data = json.load(data_file)
-                self.set_path(data['path'])
-                self.set_name(data['name'])
-                self.set_observer(data['observer'])
-                self.set_date(QDate.fromString(data['date'], Qt.ISODate))
-                self.set_equipment(data['equipment'])
-                self.raw_profiles = data['raw_profiles']
-                self.calibrated_profiles = data['calibrated_profiles']
-                self.instrument_responses = data['instrument_responses']
-                self.finished_profiles = data['finished_profiles']
-                
-    def __init_fields(self):
-        self.name = self.observer = self.position = self.equipment = ''
-        self.date = QDate.currentDate()
-        self.raw_profiles = []
-        self.calibrated_profiles = []
-        self.instrument_responses = []
-        self.finished_profiles = []
+                self.data = json.load(data_file)
         
     def set_path(self, path):
         self.path = path
+    def get_path(self):
+        return self.path
         
     def set_name(self, name):
-        self.name = name
+        self.data['name'] = name
+    def get_name(self):
+        return self.data.get('name', '')
         
     def set_observer(self, observer):
-        self.observer = observer
+        self.data['observer']= observer
+    def get_observer(self):
+        return self.data.get('observer', '')
 
     def set_position(self, position):
-        self.position = position
+        self.data['position'] = position
+    def get_position(self):
+        return self.data.get('position', '')
         
     def set_date(self, date):
-        self.date = date
-        
+        self.data['date'] = date.toString(Qt.ISODate)
+    def get_date(self):
+        return QDate.fromString(self.data.get('date', QDate.currentDate().toString(Qt.ISODate)), Qt.ISODate)
+
     def set_equipment(self, equipment):
-        self.equipment = equipment
+        self.data['equipment'] = equipment
+    def get_equipment(self):
+        return self.data.get('equipment', '')
         
+    def get_raw_profiles(self):
+        return self.data.get('raw_profiles', [])
+    
+    def get_calibrated_profiles(self):
+        return self.data.get('calibrated_profiles', [])
+    
+    def get_finished_profiles(self):
+        return self.data.get('finished_profiles', [])
+    
+    def get_instrument_responses(self):
+        return self.data.get('instrument_responses', [])
+    
+    
     def rotation_angle(self):
         pass
     
@@ -59,14 +67,4 @@ class Project:
         return os.path.join(self.path, 'project.json')
     
     def __to_JSON(self):
-        return json.dumps({
-            'name': self.name,
-            'date': self.date.toString(Qt.ISODate),
-            'observer': self.observer,
-            'position': self.position,
-            'equipment': self.equipment,
-            'raw_profiles': self.raw_profiles,
-            'calibrated_profiles': self.calibrated_profiles,
-            'instrument_responses': self.instrument_responses,
-            'finished_profiles': self.finished_profiles,
-            }, sort_keys=False, indent=4)
+        return json.dumps(self.data, sort_keys=False, indent=4)
